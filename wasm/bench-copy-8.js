8680e2919901e6426a7a3a88f543951a901af867
Ryan Hunt <rhunt@eqrion.net>
2019-09-05 20:02:35 -0500

    Update wasm tests

---
+++ wasm/bench-copy-8.js
@@ -0,0 +1,14 @@
+// Do 500 million memory.copy's.
+print("Start");
+
+const kIterations = 50000000;
+let bin = read('bench-copy.wasm', 'binary');
+let instance = new WebAssembly.Instance(new WebAssembly.Module(bin));
+
+let test = instance.exports.test8;
+for (let i = 0; i < kIterations; i++) {
+    test();
+}
+
+print("End");
+quit();
